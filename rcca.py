9802c951d46855de711443c0e5cee2dc4c3e0e45
Naoya Iwamoto <iwanao731@gmail.com>
2017-06-06 17:19:51 +0900

    Update rcca.py

--- rcca.py
+++ rcca.py
@@ -1,123 +1,159 @@
-import csv
 import numpy as np
+from scipy.linalg import svd
+from sklearn.metrics.pairwise import pairwise_kernels, euclidean_distances
+import csv
 import pandas as pd
-from scipy.linalg import sqrtm
 
-# polynomial kernel (two degree)
-def kernelFunction(a,b):
-	w = np.matmul(a.T, b)
-	print("kernel value : ", w) 
-	return pow(w, 2)
+class KCCA(object):
+	def __init__(self, n_components=1, epsilon=1.0, kernel="linear", degree=3, gamma=None, coef0=1, n_jobs=1):
+		self.n_components = n_components
+		self.epsilon = epsilon
+		self.kernel = kernel
+		self.degree = degree
+		self.gamma = gamma
+		self.coef0 = coef0
+		self.n_jobs = n_jobs
 
-def optimizedProjection(Dr, D):
-	InvDrDrT = np.linalg.inv(np.matmul(Dr, Dr.T))
-	DrDT = np.matmul(Dr, D.T)
-	return np.matmul(InvDrDrT, DrDT)
+	def _pairwise_kernels(self, X, Y=None):
+		return pairwise_kernels(X, Y, metric=self.kernel, filter_params=True, n_jobs=self.n_jobs, degree=self.degree, gamma=self.gamma, coef0=self.coef0)
 
-def readTextFiles(filename, start, end):
-	sizes = end - start
-	FrameData = np.zeros((sizes, 28))
-	for i in range(sizes):
-		f = pd.read_csv(filename + str(start + i) + '.txt', header=None)
-		Array = f.values.astype(float)
-		FrameData[i,:] = (Array[:,0])
+	def fit(self, X, Y):
+		ndata_x, nfeature_x = X.shape	# dimention such as (2, 3)
+		ndata_y, nfeature_y = Y.shape
+		if ndata_x != ndata_y:
+			raise Exception("Inequality of number of data between X and Y")
 
-#================================
-# Offline Process
-#================================
+		print("ndata_x", ndata_x)
+		print("nfeature_x", nfeature_x)
+		print("nfeature_y", nfeature_y)
 
-readTextFiles('Coeff\BSCoeff', 31, 50)
+		if self.kernel != "precomputed":
+			Kx = self._pairwise_kernels(X)
+			Ky = self._pairwise_kernels(Y)
 
-# loading source and target weights files
-## source
-df = pd.read_csv('sourceWeights.csv', header=None)
-C = df.values.astype(float)
-print("source shape : ", C.shape)
-# print(C)
+		print("Kx \n", Kx)
+		print("Ky \n", Ky)
 
-## target
-df = pd.read_csv('targetWeights.csv', header=None)
-D = df.values.astype(float)
-print("target shape : ", D.shape)
-# print(D)
+		I = self.epsilon * np.identity(ndata_x)
 
-## modified oritinal data to compute inverse matrix for C and D
-C += np.random.random(C.shape) / 10.0
-D += np.random.random(D.shape) / 10.0
+		print("I \n", I)
 
-# define the sourace and target dimention
-# NS : Number of source weights
-# NT : Number of target weights
-# NP : Number of pair data
-# NF : Number of reduced dimension
-NS, NP = C.shape
-NT, NP = D.shape
-NF = 10
+		KxI_inv = np.linalg.inv(Kx + I)
+		KyI_inv = np.linalg.inv(Ky + I)
 
-# kernelized vector in eq. (6)
-Kc = kernelFunction(C,C)
-Kd = np.matmul(D.T, D)
-print("Kd : \n", Kd)
+		print("KxI_inv \n", KxI_inv)
+		print("KyI_inv \n", KyI_inv)
 
-# Fc, Fd
-Kc2 = np.matmul(Kc, Kc)
-Kd2 = np.matmul(Kd, Kd)
-InvSqrtKc2 = np.linalg.inv(sqrtm(Kc2))
-InvSqrtKd2 = np.linalg.inv(sqrtm(Kd2))
-KcKd = np.matmul(Kc, Kd)
-UDVT = np.matmul(np.matmul(InvSqrtKc2, KcKd), InvSqrtKd2)
-U, s, V = np.linalg.svd(UDVT, full_matrices=True)
-Fc = np.matmul(InvSqrtKc2, U)
-Fd = np.matmul(InvSqrtKd2, V)
+		L = np.dot(KxI_inv, np.dot(Kx, np.dot(Ky, KyI_inv)))
 
-# reduce
-Fc = Fc[:NF, :]
-Fd = Fd[:NF, :]
-print("Fc : ", Fc.shape)
-print("Fd : ", Fd.shape)
-# print("Fc : ", Fc)
+		print("L \n", L)
 
-# Cr, Dr
-kc = np.zeros((NP, NP))
-kd = np.zeros((NP, NP))
-for i in range(NP):
-	for j in range(NP):
-		kc[i][j] = kernelFunction(C[:,i],C[:,j])
-		kd[i][j] = kernelFunction(D[:,i],D[:,j])
+		U, s, Vh = svd(L)
 
-Cr = np.zeros((NF, NP))
-Dr = np.zeros((NF, NP))
-for i in range(NP):
-	Cr[:,i] = (np.matmul(Fc, kc[i])).T
-	Dr[:,i] = (np.matmul(Fd, kc[i])).T
+		print("U \n", U)
+		print("s \n", s)
+		print("Vh \n", Vh)
 
-# Bc
-Bc = optimizedProjection(Cr, Dr)
-print("Bc", Bc.shape)
-print("Bc : ", Bc)
+		self.alpha = np.dot(KxI_inv, U[:, :self.n_components])
+		self.beta = np.dot(KyI_inv, Vh.T[:, :self.n_components])
 
-# Hd
-Hd = optimizedProjection(Dr, D)
-print("Hd", Hd.shape)
-print("Hd : ", Hd)
+		print("Fc \n", self.alpha)
+		print("Fd \n", self.beta)
 
-# Md
-Md = np.matmul(np.matmul(Hd.T, Bc), Fc)
-print("Md", Md.shape)
-print(Md)
+		return self
 
-#================================
-# Online Process
-#================================
+if __name__ == "__main__":
 
-# c_new = C[:,5]
-c_new = np.random.random((NS, 1))
-print("c_new ; \n", c_new)
+	NP = 15 # Number of pair dataset
+	NS = 48	# Number of source
+	NT = 14	# Number of target
+	NF = 10	# Number of reduced model
 
-kernlized_Cnew = np.zeros((NP,1))
-for i in range(NP):
-	kernlized_Cnew[i] = kernelFunction(c_new, C[:,i])
-# print(kernlized_Cnew)
+	# loading source and target weights files
+	## source
+	df = pd.read_csv('sourceWeights.csv', header=None)
+	X = df.values.astype(float)
+	X = X.T
+	print("source shape : ", X.shape)
+	# print(C)
 
-target = np.matmul(Md, kernlized_Cnew)
-print("target : \n", target)
+	## target
+	df = pd.read_csv('targetWeights.csv', header=None)
+	Y = df.values.astype(float)
+	Y = Y.T
+	print("target shape : ", Y.shape)
+	# print(D)
+
+	# X = np.random.normal(size=(NP,NS))
+	# Y = np.random.normal(size=(NP,NT))
+	kcca = KCCA(n_components=NF, kernel="linear", n_jobs=1, epsilon=0.1).fit(X, Y)
+
+	"""
+	matching on test data
+	"""
+	Fc = kcca.alpha
+	Fd = kcca.beta
+	print("Fc", Fc.shape)
+	print("Fd", Fd.shape)
+
+	# X_te = np.random.normal(size=(NP,NS))
+	# Y_te = np.random.normal(size=(NP,NT))
+	X_te = X
+	Y_te = Y
+	
+	# X_te = np.random.normal(size=(NF,NS))
+	# Y_te = np.random.normal(size=(NF,NT))
+
+	Kx = kcca._pairwise_kernels(X_te, X)
+	Ky = kcca._pairwise_kernels(Y_te, Y)
+	print("Kx", Kx.shape)
+	print("Ky", Ky.shape)
+
+	Cr = np.dot(Fc.T, Kx) 	# cr
+	Dr = np.dot(Fd.T, Ky)	# dr
+	print("Cr", Cr.shape)
+	print(Cr)
+	print("Dr", Dr.shape)
+	print(Dr)
+
+	# Bc
+	Bc = np.dot(np.linalg.inv(np.dot(Cr, Cr.T)), np.dot(Cr, Dr.T))
+	print("Bc", Bc.shape)
+	print(Bc)
+
+	# # Hd
+	Hd = np.dot(np.linalg.inv(np.dot(Dr, Dr.T)), np.dot(Dr, Y))
+	print("Hd", Hd.shape)
+	print(Hd)
+
+	# # Md
+	Md = np.dot(Hd.T, (np.dot(Bc, Fc.T)))
+	print("Md", Md.shape)
+	print(Md)
+
+	Xr = (X[1, :])
+
+	print("Xr \n", Xr)
+
+	C_new = kcca._pairwise_kernels(Xr, X).T
+
+	print("C_new, ", C_new.shape)
+	# C_new = np.random.random((NP, 1))
+	# C_new = np.random.random((NT, 1))
+	print((Md).dot(C_new))
+
+	# D = euclidean_distances(Cr, Dr)
+	# print("D", D.shape)	# Br
+
+	# idx_pred = np.argmin(D, axis=0)
+	# #print("matching result:")
+	# print(idx_pred)
+
+	# """
+	# similarity between true object and predicted object on test data
+	# """
+	# idx_true = range(10)
+	# C = pairwise_kernels(Y_te[idx_true], Y_te[idx_pred], metric="cosine")
+	# print("1-best mean similarity:")
+	# final = np.mean(C.diagonal())
+	# print(final)
